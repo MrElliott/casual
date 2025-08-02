1f4a683598981a79f76e47c7f4484688f2db1e7b
Elliott <elliottholt90@gmail.com>
2025-08-02 21:59:00 +0100

    further work on editor tooling (it's actually quite fun)

--- Assets/Editor/PlaceableEditorWindow.cs
+++ Assets/Editor/PlaceableEditorWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ScriptableObjectS;
 using UnityEditor;
 using UnityEngine;
@@ -7,6 +8,9 @@ namespace Editor
 {
     public class PlaceableEditorWindow: EditorWindow
     {
+        private Dictionary<string, bool> foldouts = new Dictionary<string, bool>();
+        private string soFilter = String.Empty;
+        
         [MenuItem("Window/Placeable Editor")]
         public static void ShowWindow()
         {
@@ -15,12 +19,10 @@ namespace Editor
 
         private void OnGUI()
         {
-            GUILayout.Label("Testing");
-
-            if (GUILayout.Button("Test"))
-            {
-                
-            }
+            EditorGUILayout.LabelField("Placeable Objects");
+            EditorGUILayout.Space();
+            soFilter = EditorGUILayout.TextField("Filter: ", soFilter);
+            EditorGUILayout.Space();
             
             string[] guids = AssetDatabase.FindAssets("t:PlaceableSO");
 
@@ -29,7 +31,37 @@ namespace Editor
                 string path = AssetDatabase.GUIDToAssetPath(guid);
                 PlaceableSO so = AssetDatabase.LoadAssetAtPath<PlaceableSO>(path);
                 
-                GUILayout.Label($"ScriptableObject: {so.name}");
+                if(soFilter.Length > 0 && !so.name.Contains(soFilter, StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+                
+                foldouts.TryAdd(so.name, false);
+                
+                foldouts[so.name] = EditorGUILayout.Foldout(foldouts[so.name], so.name);
+
+                if (foldouts[so.name])
+                {
+                    GUILayout.Label($"ScriptableObject: {so.name}");
+                    
+                    // Create a SerializedObject for the ScriptableObject
+                    SerializedObject serializedObject = new SerializedObject(so);
+                    SerializedProperty property = serializedObject.GetIterator();
+            
+                    // Iterate through all serialized properties
+                    property.NextVisible(true); // Skip the "m_Script" property
+                    while (property.NextVisible(false))
+                    {
+                        // Display each property
+                        EditorGUILayout.PropertyField(property, true);
+                    }
+            
+                    serializedObject.ApplyModifiedProperties();
+
+                    
+                    if (GUILayout.Button("I am happy to be here!"))
+                    {
+                
+                    }
+                }
             }
         }
     }
